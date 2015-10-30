d84ae744d5c7016ed48946861b15fb6ea9da4c85
Muralidharan <muraliselva.10@gmail.com>
2015-10-30 18:59:52 +0530

    deleted unwanted lines

--- designate/storage/impl_sqlalchemy/__init__.py
+++ designate/storage/impl_sqlalchemy/__init__.py
@@ -13,39 +13,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from oslo.config import cfg
-from designate.openstack.common import log as logging
-from designate.storage.base import Storage
-
-LOG = logging.getLogger(__name__)
-
-
-def get_storage():
-    """ Return the engine class from the provided engine name """
-    storage_driver = cfg.CONF['service:central'].storage_driver
-
-    cls = Storage.get_driver(storage_driver)
-
-    return cls()
-root@newds:~/designate/designate/storage# cd impl_sqlalchemy/
-root@newds:~/designate/designate/storage/impl_sqlalchemy# 
-root@newds:~/designate/designate/storage/impl_sqlalchemy# 
-root@newds:~/designate/designate/storage/impl_sqlalchemy# cat __init__.py
-# Copyright 2012 Managed I.T.
-#
-# Author: Kiall Mac Innes <kiall@managedit.ie>
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
 import time
 import threading
 import re
