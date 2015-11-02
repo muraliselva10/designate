29c8f1279da52011b940f0c0ee92b76ad2e0ce22
Muralidharan <muraliselva.10@gmail.com>
2015-11-02 20:14:33 +0530

    original file by our setup(production)
    original file by our setup(production)

--- designate/api/v1/domains.py
+++ designate/api/v1/domains.py
@@ -14,31 +14,18 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import flask
-from oslo_log import log as logging
-
+from designate.openstack.common import log as logging
 from designate import schema
-from designate.api.v1 import load_values
 from designate.central import rpcapi as central_rpcapi
-from designate import objects
+from designate.api.v1 import load_values
 
 
 LOG = logging.getLogger(__name__)
+central_api = central_rpcapi.CentralAPI()
 blueprint = flask.Blueprint('domains', __name__)
 domain_schema = schema.Schema('v1', 'domain')
 domains_schema = schema.Schema('v1', 'domains')
 servers_schema = schema.Schema('v1', 'servers')
-
-
-def _pool_ns_record_to_server(pool_ns_record):
-    server_values = {
-        'id': pool_ns_record.id,
-        'created_at': pool_ns_record.created_at,
-        'updated_at': pool_ns_record.updated_at,
-        'version': pool_ns_record.version,
-        'name': pool_ns_record.hostname
-    }
-
-    return objects.Server.from_dict(server_values)
 
 
 @blueprint.route('/schemas/domain', methods=['GET'])
@@ -59,13 +46,7 @@ def create_domain():
     values = load_values(flask.request, valid_attributes)
 
     domain_schema.validate(values)
-
-    central_api = central_rpcapi.CentralAPI.get_instance()
-
-    # A V1 zone only supports being a primary (No notion of a type)
-    values['type'] = 'PRIMARY'
-
-    domain = central_api.create_domain(context, objects.Domain(**values))
+    domain = central_api.create_domain(context, values)
 
     response = flask.jsonify(domain_schema.filter(domain))
     response.status_int = 201
@@ -78,9 +59,7 @@ def create_domain():
 def get_domains():
     context = flask.request.environ.get('context')
 
-    central_api = central_rpcapi.CentralAPI.get_instance()
-
-    domains = central_api.find_domains(context, criterion={"type": "PRIMARY"})
+    domains = central_api.find_domains(context)
 
     return flask.jsonify(domains_schema.filter({'domains': domains}))
 
@@ -89,10 +68,7 @@ def get_domains():
 def get_domain(domain_id):
     context = flask.request.environ.get('context')
 
-    central_api = central_rpcapi.CentralAPI.get_instance()
-
-    criterion = {"id": domain_id, "type": "PRIMARY"}
-    domain = central_api.find_domain(context, criterion=criterion)
+    domain = central_api.get_domain(context, domain_id)
 
     return flask.jsonify(domain_schema.filter(domain))
 
@@ -102,22 +78,12 @@ def update_domain(domain_id):
     context = flask.request.environ.get('context')
     values = flask.request.json
 
-    central_api = central_rpcapi.CentralAPI.get_instance()
-
-    # Fetch the existing resource
-    criterion = {"id": domain_id, "type": "PRIMARY"}
-    domain = central_api.find_domain(context, criterion=criterion)
-
-    # Prepare a dict of fields for validation
-    domain_data = domain_schema.filter(domain)
-    domain_data.update(values)
-
-    # Validate the new set of data
-    domain_schema.validate(domain_data)
-
-    # Update and persist the resource
+    domain = central_api.get_domain(context, domain_id)
+    domain = domain_schema.filter(domain)
     domain.update(values)
-    domain = central_api.update_domain(context, domain)
+
+    domain_schema.validate(domain)
+    domain = central_api.update_domain(context, domain_id, values)
 
     return flask.jsonify(domain_schema.filter(domain))
 
@@ -125,12 +91,6 @@ def update_domain(domain_id):
 @blueprint.route('/domains/<uuid:domain_id>', methods=['DELETE'])
 def delete_domain(domain_id):
     context = flask.request.environ.get('context')
-
-    central_api = central_rpcapi.CentralAPI.get_instance()
-
-    # TODO(ekarlso): Fix this to something better.
-    criterion = {"id": domain_id, "type": "PRIMARY"}
-    central_api.find_domain(context, criterion=criterion)
 
     central_api.delete_domain(context, domain_id)
 
@@ -141,17 +101,6 @@ def delete_domain(domain_id):
 def get_domain_servers(domain_id):
     context = flask.request.environ.get('context')
 
-    central_api = central_rpcapi.CentralAPI.get_instance()
-
-    # TODO(ekarlso): Fix this to something better.
-    criterion = {"id": domain_id, "type": "PRIMARY"}
-    central_api.find_domain(context, criterion=criterion)
-
-    nameservers = central_api.get_domain_servers(context, domain_id)
-
-    servers = objects.ServerList()
-
-    for ns in nameservers:
-        servers.append(_pool_ns_record_to_server(ns))
+    servers = central_api.get_domain_servers(context, domain_id)
 
     return flask.jsonify(servers_schema.filter({'servers': servers}))
