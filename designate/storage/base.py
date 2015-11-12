d3aada93e3c060bbe70c2e281f05db1f8c2bade2
Muralidharan <muraliselva.10@gmail.com>
2015-11-12 23:22:34 +0530

    modifications done for create record - Phase 1
    modifications done for create record only based on Tenant (modifications of record listing removed to avoid conflict)

--- designate/storage/base.py
+++ designate/storage/base.py
@@ -1,21 +1,21 @@
 # Copyright 2012 Managed I.T.
 #
-# Author: Kiall Mac Innes <kiall@managedit.ie>                                                                                                                                                             
-#                                                                                                                                                                                                          
-# Licensed under the Apache License, Version 2.0 (the "License"); you may                                                                                                                                  
-# not use this file except in compliance with the License. You may obtain                                                                                                                                  
-# a copy of the License at                                                                                                                                                                                 
-#                                                                                                                                                                                                          
-#      http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                                          
-#                                                                                                                                                                                                          
-# Unless required by applicable law or agreed to in writing, software                                                                                                                                      
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT                                                                                                                                
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the                                                                                                                                 
-# License for the specific language governing permissions and limitations                                                                                                                                  
-# under the License.                                                                                                                                                                                       
-import abc                                                                                                                                                                                                 
-from designate.plugin import DriverPlugin                                                                                                                                                                  
-                                                                                                                                                                                                           
+# Author: Kiall Mac Innes <kiall@managedit.ie>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import abc
+from designate.plugin import DriverPlugin
+
 
 class Storage(DriverPlugin):
 
@@ -364,6 +364,17 @@ class Storage(DriverPlugin):
         :param domain_id: Domain ID to create the recordset in.
         :param values: Values to create the new RecordSet from.
         """
+
+    @abc.abstractmethod
+    def create_recordset_ptr(self, context, domain_id, values):
+        """
+        Create a recordset on a given Domain ID
+
+        :param context: RPC Context.
+        :param domain_id: Domain ID to create the recordset in.
+        :param values: Values to create the new RecordSet from.
+        """
+
 
     @abc.abstractmethod
     def get_recordset(self, context, recordset_id):
