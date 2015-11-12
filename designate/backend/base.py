64aa689222acbadbfb53cdda7c289a7c1b0ea2f9
Muralidharan <muraliselva.10@gmail.com>
2015-11-12 23:26:48 +0530

    base.py producation version
    base.py producation version

--- designate/backend/base.py
+++ designate/backend/base.py
@@ -14,91 +14,137 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import abc
-
-from oslo_config import cfg
-from oslo_log import log as logging
-
-from designate.i18n import _LI
+from designate.openstack.common import log as logging
+from designate import exceptions
 from designate.context import DesignateContext
 from designate.plugin import DriverPlugin
-from designate.mdns import rpcapi as mdns_api
 
 
 LOG = logging.getLogger(__name__)
-CONF = cfg.CONF
-
-CONF.import_opt('pool_id', 'designate.pool_manager',
-                group='service:pool_manager')
 
 
 class Backend(DriverPlugin):
-    """Base class for backend implementations"""
+    """ Base class for backend implementations """
     __plugin_type__ = 'backend'
     __plugin_ns__ = 'designate.backend'
 
-    def __init__(self, target):
+    def __init__(self, central_service):
         super(Backend, self).__init__()
-
-        self.target = target
-        self.options = target.options
-        self.masters = target.masters
-        self.host = self.options.get('host', '127.0.0.1')
-        self.port = int(self.options.get('port', 53))
-
-        # TODO(kiall): Context's should never be shared across requests.
+        self.central_service = central_service
         self.admin_context = DesignateContext.get_admin_context()
         self.admin_context.all_tenants = True
 
-        # Options for sending NOTIFYs
-        self.timeout = CONF['service:pool_manager'].poll_timeout
-        self.retry_interval = CONF['service:pool_manager'].poll_retry_interval
-        self.max_retries = CONF['service:pool_manager'].poll_max_retries
-        self.delay = CONF['service:pool_manager'].poll_delay
-
     def start(self):
-        LOG.info(_LI('Starting %s backend'), self.get_canonical_name())
+        pass
 
     def stop(self):
-        LOG.info(_LI('Stopped %s backend'), self.get_canonical_name())
+        pass
 
-    @property
-    def mdns_api(self):
-        return mdns_api.MdnsAPI.get_instance()
+    def create_tsigkey(self, context, tsigkey):
+        """ Create a TSIG Key """
+        raise exceptions.NotImplemented(
+            'TSIG is not supported by this backend')
 
-    # Core Backend Interface
+    def update_tsigkey(self, context, tsigkey):
+        """ Update a TSIG Key """
+        raise exceptions.NotImplemented(
+            'TSIG is not supported by this backend')
+
+    def delete_tsigkey(self, context, tsigkey):
+        """ Delete a TSIG Key """
+        raise exceptions.NotImplemented(
+            'TSIG is not supported by this backend')
+
     @abc.abstractmethod
     def create_domain(self, context, domain):
-        """
-        Create a DNS domain.
+        """ Create a DNS domain """
 
-        :param context: Security context information.
-        :param domain: the DNS domain.
-        """
-
+    @abc.abstractmethod
     def update_domain(self, context, domain):
-        """
-        Update a DNS domain.
-
-        :param context: Security context information.
-        :param domain: the DNS domain.
-        """
-        LOG.debug('Update Domain')
-
-        self.mdns_api.notify_zone_changed(
-            context, domain, self.host, self.port, self.timeout,
-            self.retry_interval, self.max_retries, self.delay)
+        """ Update a DNS domain """
 
     @abc.abstractmethod
     def delete_domain(self, context, domain):
-        """
-        Delete a DNS domain.
+        """ Delete a DNS domain """
 
-        :param context: Security context information.
-        :param domain: the DNS domain.
+    def create_recordset(self, context, domain, recordset):
+        """ Create a DNS recordset """
+
+    @abc.abstractmethod
+    def update_recordset(self, context, domain, recordset):
+        """ Update a DNS recordset """
+
+    @abc.abstractmethod
+    def delete_recordset(self, context, domain, recordset):
+        """ Delete a DNS recordset """
+
+    @abc.abstractmethod
+    def create_record(self, context, domain, recordset, record):
+        """ Create a DNS record """
+
+    @abc.abstractmethod
+    def update_record(self, context, domain, recordset, record):
+        """ Update a DNS record """
+
+    @abc.abstractmethod
+    def delete_record(self, context, domain, recordset, record):
+        """ Delete a DNS record """
+
+    @abc.abstractmethod
+    def create_server(self, context, server):
+        """ Create a DNS server """
+
+    @abc.abstractmethod
+    def update_server(self, context, server):
+        """ Update a DNS server """
+
+    @abc.abstractmethod
+    def delete_server(self, context, server):
+        """ Delete a DNS server """
+
+    def sync_domain(self, context, domain, records):
         """
+        Re-Sync a DNS domain
+
+        This is the default, naive, domain synchronization implementation.
+        """
+        # First up, delete the domain from the backend.
+        try:
+            self.delete_domain(context, domain)
+        except exceptions.DomainNotFound as e:
+            # NOTE(Kiall): This means a domain was missing from the backend.
+            #              Good thing we're doing a sync!
+            LOG.warn("Failed to delete domain '%s' during sync. Message: %s",
+                     domain['id'], str(e))
+
+        # Next, re-create the domain in the backend.
+        self.create_domain(context, domain)
+
+        # Finally, re-create the records for the domain.
+        for record in records:
+            # Re-create the record in the backend.
+            self.create_record(context, domain, record)
+
+    def sync_record(self, context, domain, record):
+        """
+        Re-Sync a DNS record.
+
+        This is the default, naive, record synchronization implementation.
+        """
+        # First up, delete the record from the backend.
+        try:
+            self.delete_record(context, domain, record)
+        except exceptions.RecordNotFound as e:
+            # NOTE(Kiall): This means a record was missing from the backend.
+            #              Good thing we're doing a sync!
+            LOG.warn("Failed to delete record '%s' in domain '%s' during sync."
+                     " Message: %s", record['id'], domain['id'], str(e))
+
+        # Finally, re-create the record in the backend.
+        self.create_record(context, domain, record)
 
     def ping(self, context):
-        """Ping the Backend service"""
+        """ Ping the Backend service """
 
         return {
             'status': None
